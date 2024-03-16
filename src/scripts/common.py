dd56b64a32555a1ec25493f4ba9bfc26486ae4e5
Dounia <benyakhlaf.dounia@gmail.com>
2024-03-17 00:08:15 +0100

    updating common.py file due to additional mix data

--- src/scripts/common.py
+++ src/scripts/common.py
@@ -1,2 +1,5 @@
-GRO_FILE = "src/data/md.gro"
-XTC_FILE = "src/data/md_OK_dt100_zOK.xtc"
+SINGLE_GRO_FILE = "src/data/md.gro"
+SINGLE_XTC_FILE = "src/data/md_OK_dt100_zOK.xtc"
+
+MIX_GRO_FILE = "src/data/md_mixte.gro"
+MIX_XTC_FILE = "src/data/md_mixte_OK_dt100_zOK.xtc"
