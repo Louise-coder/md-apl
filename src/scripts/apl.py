bd1c880a54b112c96951c267d964a229a8d210ba
Louise <louloulam1606@gmail.com>
2024-03-17 12:45:34 +0100

    Correcting the code for mix membranes.

--- src/scripts/apl.py
+++ src/scripts/apl.py
@@ -8,10 +8,17 @@ import matplotlib.pyplot as plt
 from MDAnalysis import Universe, AtomGroup
 from numpy import ndarray
 from scipy.spatial import Voronoi, voronoi_plot_2d
-from shapely.geometry import Polygon, box
+from shapely.geometry import Polygon, box, Point
 from typing import Tuple, List
 
-from common import SINGLE_GRO_FILE, SINGLE_XTC_FILE, MIX_GRO_FILE, MIX_XTC_FILE
+from common import (
+    SINGLE_GRO_FILE,
+    SINGLE_XTC_FILE,
+    MIX_GRO_FILE,
+    MIX_XTC_FILE,
+)
+
+CHOICE = -1
 
 
 def is_region_out(region_vertices: List, box_dimensions: ndarray) -> bool:
@@ -56,7 +63,8 @@ def get_areas(my_voronoi: Voronoi, box_dimensions: ndarray) -> List:
     for region in my_voronoi.regions:
         if -1 not in region and len(region) > 0:
             region_vertices = [
-                my_voronoi.vertices[vertice_index] for vertice_index in region
+                my_voronoi.vertices[vertice_index]
+                for vertice_index in region
             ]
             region_out = is_region_out(region_vertices, box_dimensions)
             if not region_out:
@@ -65,8 +73,34 @@ def get_areas(my_voronoi: Voronoi, box_dimensions: ndarray) -> List:
     return my_areas
 
 
-def get_areas_2(my_voronoi: Voronoi, box_dimensions: ndarray) -> List:
-    """Compute the areas of the Voronoi Diagram's cells.
+def is_inside_polygon(my_points: List, my_polygon: Polygon) -> bool:
+    """Whereas one of the points is in a polygon.
+
+    Parameters
+    ----------
+    my_points : List
+        The list of points to verify.
+    my_polygon : Polygon
+        The polygon of interest.
+
+    Returns
+    -------
+    bool
+        If one of the points is in the polygon.
+    """
+    if CHOICE == 2:
+        for point in my_points:
+            if my_polygon.contains(Point(point)):
+                return True
+        return False
+    else:
+        return True
+
+
+def get_areas_2(
+    my_voronoi: Voronoi, box_dimensions: ndarray, dmpc_p_up: AtomGroup
+) -> Tuple:
+    """Compute the areas of the Voronoi Diagram's cells for DMPC and DMPG.
 
     Parameters
     ----------
@@ -74,27 +108,36 @@ def get_areas_2(my_voronoi: Voronoi, box_dimensions: ndarray) -> List:
         The Voronoi diagram of interest.
     box_dimensions : ndarray
         The dimensions of the simulation box.
+    dmpc_p_up : AtomGroup
+        The selection of DMPC P atoms.
 
     Returns
     -------
-    List
-        The computed list of areas.
+    dmpc_areas, dmpg_areas : Tuple(List, List)
+        The computed list of areas for DMPC and DMPG lipids.
     """
-    my_areas = []
+    dmpc_areas, dmpg_areas = [], []
     my_box = box(0, 0, box_dimensions[0], box_dimensions[1])
     for region in my_voronoi.regions:
         if -1 not in region and len(region) > 0:
             region_vertices = [
-                my_voronoi.vertices[vertice_index] for vertice_index in region
+                my_voronoi.vertices[vertice_index]
+                for vertice_index in region
             ]
             polygon = Polygon(region_vertices)
             intersection = polygon.intersection(my_box)
-            my_areas.append(intersection.area)
-    return my_areas
+            is_dmpc = is_inside_polygon(
+                dmpc_p_up.positions[:, 0:2], intersection
+            )
+            if is_dmpc:
+                dmpc_areas.append(intersection.area)
+            else:
+                dmpg_areas.append(intersection.area)
+    return dmpc_areas, dmpg_areas
 
 
 def get_voronoi(
-    p_layer: ndarray, frame_index: int, nb_frame: int, type: str
+    p_layer: ndarray, frame_index: int, nb_frame: int
 ) -> Voronoi:
     """Compute Voronoi tesselations on a set of coordinates.
 
@@ -102,8 +145,6 @@ def get_voronoi(
     ----------
     p_layer : ndarray
         For a layer, the array containing the coordinates of P atoms.
-    type : str
-        The type of lipid.
 
     Returns
     -------
@@ -113,8 +154,8 @@ def get_voronoi(
     my_voronoi = Voronoi(p_layer)
     if frame_index in (0, nb_frame - 1):
         voronoi_plot_2d(my_voronoi)
-        plt.title(f"{type} Voronoi diagram for the frame nb " + str(frame_index))
-        plt.savefig("src/fig/" + type + "_voronoi_diagram_" + str(frame_index))
+        plt.title("Voronoi diagram for the frame nb " + str(frame_index))
+        plt.savefig("src/fig/voronoi_diagram_" + str(frame_index))
         print("\033[90m* A Voronoi diagram has been saved in fig/\033[0m")
         plt.close()
     return my_voronoi
@@ -140,40 +181,44 @@ def get_p_from_layer(all_p_atoms: AtomGroup) -> Tuple:
     return p_up, p_down
 
 
-def get_frame_apl(
-    universe: Universe, p_atoms: AtomGroup, frame_index: int, type: str
-) -> float:
-    """Given a frame, compute the apl.
+def get_frame_apl(universe: Universe, frame_index: int) -> Tuple:
+    """Given a frame, compute the apl for DMPC and DMPG lipids.
 
     Parameters
     ----------
     universe : Universe
         The universe of interest.
-    p_atoms : AtomGroup
-        The selected P atoms.
     frame_index : int
         The index of the frame of interest.
-    type : str
-        The type of lipid.
 
     Returns
     -------
-    frame_apl : float
-        The computed apl in nm2.
+    dmpc_frame_apl, dmpg_frame_apl : Tuple(float, float)
+        The computed apl in nm² for both types of lipids.
 
     Notes
     -----
     The conversion factor is set to 100 because 1nm² = 100Å².
     """
-    p_up, _ = get_p_from_layer(p_atoms)
-
+    all_p_atoms = universe.select_atoms("name P")
+    all_p_up, _ = get_p_from_layer(all_p_atoms)
+    dmpc_p_up = all_p_atoms.select_atoms("resname DMPC")
+    dmpc_frame_apl, dmpg_frame_apl = -1, -1
     my_voronoi = get_voronoi(
-        p_up.positions[:, 0:2], frame_index, len(universe.trajectory), type
+        all_p_up.positions[:, 0:2], frame_index, len(universe.trajectory)
     )
-    my_areas = get_areas_2(my_voronoi, universe.dimensions)
+    dmpc_areas, dmpg_areas = get_areas_2(
+        my_voronoi, universe.dimensions, dmpc_p_up
+    )
     conversion_factor = 100
-    frame_apl = sum(my_areas) / (len(my_areas) * conversion_factor)
-    return frame_apl
+    dmpc_frame_apl = sum(dmpc_areas) / (
+        len(dmpc_areas) * conversion_factor
+    )
+    if CHOICE == 2:
+        dmpg_frame_apl = sum(dmpg_areas) / (
+            len(dmpg_areas) * conversion_factor
+        )
+    return dmpc_frame_apl, dmpg_frame_apl
 
 
 def get_apl_distribution(all_frame_apl: List, type: str):
@@ -195,7 +240,7 @@ def get_apl_distribution(all_frame_apl: List, type: str):
     plt.ylabel("APL (nm²)")
     plt.title(f"Variation of {type} APL values across trajectory frames")
     plt.savefig("src/fig/" + type + "_apl_distribution")
-    print("\033[90m* The APL distribution has been saved in fig/\033[0m\n")
+    print("\033[90m* The APL distribution has been saved in fig/\033[0m")
     plt.close()
 
 
@@ -212,24 +257,22 @@ def get_trajectory_apl(universe: Universe) -> Tuple:
     dmpc_apl, dmpg_apl : Tuple
         The DMPC and DMPG apl in a tuple.
     """
-    dmpc_frame_apl = []
-    dmpg_frame_apl = []
+    dmpc_list_apl, dmpg_list_apl = [], []
     dmpc_apl, dmpg_apl = -1, -1
     for my_frame in universe.trajectory:
-        dmpc_p_atoms = universe.select_atoms("resname DMPC and name P")
-        dmpg_p_atoms = universe.select_atoms("resname DMPG and name P")
-        if dmpc_p_atoms:
-            frame_apl = get_frame_apl(universe, dmpc_p_atoms, my_frame.frame, "DMPC")
-            dmpc_frame_apl.append(frame_apl)
-        if dmpg_p_atoms:
-            frame_apl = get_frame_apl(universe, dmpg_p_atoms, my_frame.frame, "DMPG")
-            dmpg_frame_apl.append(frame_apl)
-    if dmpc_frame_apl:
-        get_apl_distribution(dmpc_frame_apl, "DMPC")
-        dmpc_apl = sum(dmpc_frame_apl) / len(dmpc_frame_apl)
-    if dmpg_frame_apl:
-        get_apl_distribution(dmpg_frame_apl, "DMPG")
-        dmpg_apl = sum(dmpg_frame_apl) / len(dmpg_frame_apl)
+        if my_frame.frame % 100 == 0:
+            print(f".......... FRAME {my_frame.frame} ..........")
+        dmpc_frame_apl, dmpg_frame_apl = get_frame_apl(
+            universe, my_frame.frame
+        )
+        dmpc_list_apl.append(dmpc_frame_apl)
+        if CHOICE == 2:
+            dmpg_list_apl.append(dmpg_frame_apl)
+    get_apl_distribution(dmpc_list_apl, "DMPC")
+    dmpc_apl = sum(dmpc_list_apl) / len(dmpc_list_apl)
+    if CHOICE == 2:
+        get_apl_distribution(dmpg_list_apl, "DMPG")
+        dmpg_apl = sum(dmpg_list_apl) / len(dmpg_list_apl)
     return dmpc_apl, dmpg_apl
 
 
@@ -248,7 +291,7 @@ def display_info(universe: Universe):
     nb_atoms = len(universe.atoms)
     box_dimensions = universe.dimensions[0:3]
     nb_frames = len(universe.trajectory)
-    print(f"Number of atoms:\t{nb_atoms}")
+    print(f"\nNumber of atoms:\t{nb_atoms}")
     print(f"Number of frames:\t{nb_frames}")
     print(f"Box dimensions:\t\t{box_dimensions}\n")
     print(f"\033[92mLoading...\033[0m\n")
@@ -267,7 +310,7 @@ def get_choice():
     while not is_ok:
         choice = input(
             "Which type of membrane do you want to compute the apl from?\n\
-[1] Single (MPC)\n[2] Mix (MPC and MPG)\n"
+[1] Single (MPC)\n[2] Mix (MPC and MPG)\nChoice: "
         )
         try:
             int_choice = int(choice)
@@ -281,16 +324,16 @@ def get_choice():
 if __name__ == "__main__":
     print("\n\033[95m========== APL Calculator ==========\033[0m")
     choice = get_choice()
-
     if choice == 1:
         my_universe = Universe(SINGLE_GRO_FILE, SINGLE_XTC_FILE)
     else:
         my_universe = Universe(MIX_GRO_FILE, MIX_XTC_FILE)
+    CHOICE = choice
     display_info(my_universe)
     dmpc_apl, dmpg_apl = get_trajectory_apl(my_universe)
     if dmpc_apl != -1:
         print(
-            f"Computed APL DMPC for this trajectory : \033[94m{dmpc_apl:.3f} nm²\033[0m\n"
+            f"\nComputed APL DMPC for this trajectory : \033[94m{dmpc_apl:.3f} nm²\033[0m\n"
         )
     if dmpg_apl != -1:
         print(
