39140b084d601f85c0a8da2957fcb2d7239e524b
Dounia <benyakhlaf.dounia@gmail.com>
2024-03-17 22:11:20 +0100

    Adding the experimental values

--- src/scripts/common.py
+++ src/scripts/common.py
@@ -3,3 +3,6 @@ SINGLE_XTC_FILE = "src/data/md_OK_dt100_zOK.xtc"
 
 MIX_GRO_FILE = "src/data/md_mixte.gro"
 MIX_XTC_FILE = "src/data/md_mixte_OK_dt100_zOK.xtc"
+
+EXP_DMPC_VALUE = 0.606
+EXP_DMPG_VALUE = 0.630
